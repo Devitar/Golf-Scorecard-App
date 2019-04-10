3244db0a6f521d5eae0359728ae73f2b21f020c0
Devin Curtis <devin.curtis1210@gmail.com>
2019-04-10 06:59:53 -0600

    OOP JS Overhaul

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,38 +1,17 @@
-// var courses;
-
-// getAllCourses();
-
-// function getAllCourses() {
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             courses = JSON.parse(this.responseText);
-//             initialize();
-//         }
-//     };
-//     xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
-//     xhttp.setRequestHeader("ContentType", "application/json");
-//     xhttp.send();
-// }
-
-// function initialize(){
-//     console.log("Started code. ", courses);
-// }
-
 //$('.navbar').effect('shake', {}, 500)
 
-let allcats;
 let selcourse;
 let seltype;
 
-//Modal
+//Modal, initial load
 let mainModal = $(".modal");
 let closeModal = $("#closeModal");
 let loadingGif = $("#loadingGif");
 $("#loadingGif").ready(function(){
     showModal();
     loadingGif.css('width', loadingGif.height());
-    getCourses();
+    // getCourses();
+    allCourses.loadInitialData();
 });
 
 function showModal(){
@@ -43,72 +22,9 @@ function hideModal(){
 }
 //
 
-function getCourses() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            hideModal();
-            allcats = JSON.parse(this.responseText);
-            console.log(allcats);
-            allcats.courses.forEach((v, i) => {
-                let newItem = `<div id="${v.id}" class="courseContainer card">` +
-                    `<img src="${v.image}" class="courseImg card-img-top" alt="Course Image" />` +
-                    `<div class="card-body">` + 
-                    `<p class="card-text">` + v.name + `</p>` +
-                    `<button onclick="returnCourse(${v.id}, this)" class="courseButton">More Info</button>` +
-                    `</div>` +
-                    `</div>`;
-                $(".centerbox").append(newItem);
-            });
-        }
-    }
-    xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
-    xhttp.send();
-}
-
 function returnCourse(courseid, mybtn) {
-    showModal();
-    let thecard = $(mybtn).parent();
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            hideModal();
-            selcourse = JSON.parse(this.responseText);
-            let teeBoxArray = [];
-            let dropDownMenu = `<select id="${selcourse.data.id}" class="dropdownMenu" onchange="setTee(value)">`
-
-            console.log(selcourse);
-            let teesArray = selcourse.data.holes[0].teeBoxes;
-            teesArray.forEach((v, i) => {
-                if (v.teeType != "auto change location"){ //Excludes the auto distance tee (unsure what it is)
-                    let teeType = v.teeType;
-                    let newString = teeType.charAt(0).toUpperCase() + teeType.slice(1);
-                    teeBoxArray.push(`<option value="${teeType}">${newString}</option>`)
-                };
-            });
-            teeBoxArray.forEach((v,i) => {
-                dropDownMenu += v;
-            });
-            dropDownMenu += "</select>";
-            $(mybtn).hide();
-            $(`#${courseid}`).append(dropDownMenu);
-        }
-    }
-    xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses/" + courseid, true);
-    xhttp.send();
+    allCourses.loadCourse(courseid, mybtn);
 }
-
-function showAll(typeindex) {
-    seltype = typeindex;
-    $(".classRow").remove();
-    selcourse.classes.forEach((v, i) => {
-        $(".classdisp").append(`<div class="classRow">
-            <div>${v.levels[seltype].type}</div>
-            <div>${v.levels[seltype].teacher}</div>
-            <div>${v.levels[seltype].schedule}</div>
-        </div>`);
-    });
-};
 
 function setTee(value){
     console.log(value);
--- Scripts/model.js
+++ Scripts/model.js
@@ -1,28 +1,86 @@
-class AllCourses{
-    constructor(){
+class Course {
+    constructor(id, name) {
 
     }
 }
 
-class Course{
-    constructor(){
-
-    }
-}
-
-class Player{
-    constructor(){
-
-    }
-}
-
-class PlayerCollection{
-    constructor(){
+class AllCourses {
+    constructor() {
+        this.mainCourses = [];
         this.Collection = [];
     }
-    addPlayer(){
+    loadInitialData() {
+        let xhttp = new XMLHttpRequest();
+        let mainObj = this;
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                hideModal();
+                let coursesParsed = JSON.parse(this.responseText);
+                console.log(coursesParsed);
+                coursesParsed.courses.forEach((v, i) => {
+                    mainObj.mainCourses.push({"Name": v.name, "id": v.id})
+                    let newItem = `<div id="${v.id}" class="courseContainer card">` +
+                        `<img src="${v.image}" class="courseImg card-img-top" alt="Course Image" />` +
+                        `<div class="card-body">` +
+                        `<p class="card-text">` + v.name + `</p>` +
+                        `<button onclick="returnCourse(${v.id}, this)" class="courseButton">More Info</button>` +
+                        `</div>` +
+                        `</div>`;
+                    $(".centerbox").append(newItem);
+                });
+            }
+        }
+        xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
+        xhttp.send();
+    }
+    loadCourse(courseid, mybtn) {
+        showModal();
+        let xhttp = new XMLHttpRequest();
+        let mainObj = this;
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                hideModal();
+                selcourse = JSON.parse(this.responseText);
+                mainObj.Collection.push(new Course(selcourse.data.id, selcourse.data.name));
+                let teeBoxArray = [];
+                let dropDownMenu = `<select id="${selcourse.data.id}" class="dropdownMenu" onchange="setTee(value)">`
+
+                console.log(selcourse);
+                let teesArray = selcourse.data.holes[0].teeBoxes;
+                teesArray.forEach((v, i) => {
+                    if (v.teeType != "auto change location") { //Excludes the auto distance tee (unsure what it is)
+                        let teeType = v.teeType;
+                        let newString = teeType.charAt(0).toUpperCase() + teeType.slice(1);
+                        teeBoxArray.push(`<option value="${teeType}">${newString}</option>`)
+                    };
+                });
+                teeBoxArray.forEach((v, i) => {
+                    dropDownMenu += v;
+                });
+                dropDownMenu += "</select>";
+                $(mybtn).hide();
+                $(`#${courseid}`).append(dropDownMenu);
+            }
+        }
+        xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses/" + courseid, true);
+        xhttp.send();
+    }
+}
+
+class Player {
+    constructor() {
+
+    }
+}
+
+class PlayerCollection {
+    constructor() {
+        this.Collection = [];
+    }
+    addPlayer() {
         this.Collection.push(new Player());
     }
 }
 
 var allPlayers = new PlayerCollection();
+var allCourses = new AllCourses();
